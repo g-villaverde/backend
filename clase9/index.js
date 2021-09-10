f955dce3be8a5521252954147495990c4615737b
Gonzalo <gonvillaverde@gmail.com>
2021-09-09 23:03:46 -0300

    Entrega clase 9

---
+++ clase9/index.js
@@ -0,0 +1,58 @@
+import Express from "Express";
+import { v4 } from "uuid";
+import productos from "./productos.js";
+
+const app = Express();
+const PORT = 8080;
+const router = Express.Router();
+const server = app.listen(PORT, () => console.log('servidor inicializado'));
+
+app.use(Express.static('public'));
+
+app.get("/", (_, res) => {
+    res.json("hola")
+})
+
+app.get('/productos', (_ ,res) => {
+    res.json(productos)
+}) 
+
+app.post("/productos", (req, res) => {
+    productos.push(req.body);
+    res.status(201).json(req.body);
+});
+
+app.get('/productos/:id', (req, res) => {
+    const { id } = req.params;
+    const producto = productos.find((producto) => producto.id = id);
+    res.json(producto);
+});
+
+// Actualizar un producto
+
+app.put("/productos/actualizar/:id", (req, res) => {
+    const { id } = req.params;
+    const {title} = req.body.title;
+    const producto = productos.find((producto) => producto.id = id);
+
+    user.title = title;    
+})
+
+
+
+// Borrar un producto
+app.delete("/productos/delete/:id", (req, res) => {
+    const {id} = req.params;
+    const producto = productos.filter((producto) => producto.id = id);
+    if (!producto) {
+        return res.status(404).json({msg: "no encontrado"})
+    }  
+
+    productos = productos.filter((producto) => user.id != id)
+    
+})
+
+
+
+
+
